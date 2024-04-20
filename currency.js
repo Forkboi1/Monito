f87aa34734e5a8af1ffd4b4da7a08cab2c902567
Omar <omaromair123@gmail.com>
2024-04-20 19:40:18 +0300

    fixing js

--- currency.js
+++ currency.js
@@ -2,6 +2,7 @@ window.onload = main
 
 function main() {
     let currency = document.getElementById("currencyBox")
+    currency.selectedIndex = 0
     currency.addEventListener("change", change)
     function change() {
         var img = document.getElementById("curimg")
@@ -20,6 +21,30 @@ function main() {
         }
 
     }
+
+    baseImage = document.getElementById("post_pic")
+    var image1 = document.getElementById("1");
+    var image2 = document.getElementById("2");
+    var image3 = document.getElementById("3");
+    var image4 = document.getElementById("4");
+
+    // Add click event listeners to other images
+    image1.addEventListener("click", function () {
+
+        baseImage.setAttribute("src", image1.src);
+    });
+
+    image2.addEventListener("click", function () {
+        baseImage.setAttribute("src", image2.src);
+    });
+
+    image3.addEventListener("click", function () {
+        baseImage.src = image3.src;
+    });
+
+    image4.addEventListener("click", function () {
+        baseImage.src = image4.src;
+    });
 
 }
 
--- image_selector.js
+++ image_selector.js
@@ -1,19 +1,22 @@
 window.onload = main
 
+
 function main() {
     baseImage = document.getElementById("post_pic")
     var image1 = document.getElementById("1");
     var image2 = document.getElementById("2");
     var image3 = document.getElementById("3");
     var image4 = document.getElementById("4");
+    alert("1")
 
     // Add click event listeners to other images
     image1.addEventListener("click", function () {
-        baseImage.src = image1.src;
+
+        baseImage.setAttribute("src", image1.src);
     });
 
     image2.addEventListener("click", function () {
-        baseImage.src = image2.src;
+        baseImage.setAttribute("src", image2.src);
     });
 
     image3.addEventListener("click", function () {
